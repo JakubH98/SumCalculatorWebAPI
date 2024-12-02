ec825fa8325437d4f71419f5f00b0d5911613d9c
jakub <jakubsmail8@gmail.com>
2024-12-02 11:02:11 +0100

    Main

---
+++ SumCalculatorWebAPI/Domain/User.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+namespace SumCalculatorWebAPI.Domain
+{
+    public class User
+    {
+        [BsonId]
+        public string? ID { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+
+    }
+}
