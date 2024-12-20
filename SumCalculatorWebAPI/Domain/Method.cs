9adda5793b3d1fd4c69dad1bd19e0d3bb29ae1e8
jakub <jakubsmail8@gmail.com>
2024-12-20 13:49:51 +0100

    IDK

---
+++ SumCalculatorWebAPI/Domain/Method.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace SumCalculatorWebAPI.Domain
+{
+    public class Method : IEntity
+    {
+        [BsonId]        
+        public string? ID { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public List<Item>? ArtifactList { get; set; }
+        public List<Item>? ActorList { get; set; }
+        public List<ToDo>? TodoList { get; set; }
+    }
+    public class Item
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }  
+    }
+    public class ToDo
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool? IsDone { get; set; } = false;
+    }
+}
